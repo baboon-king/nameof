18999dea75d91e2bf2871ff4fe9c194daab6a007
BaboonKing <changfan.xu.up@gmail.com>
2025-05-21 14:11:39 +0800

    refactor: simplify type definitions in nameof function overloads

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-type O = object;
-type F = Function;
-
-function nameof<T extends F>(fn: T): string;
-function nameof<T extends O>(obj: T): T extends O ? keyof T : never;
-function nameof<T extends O, K extends keyof T = keyof T>(key: K): K;
-function nameof<T extends O, K extends keyof T = keyof T>(obj: T, key: K): K;
+function nameof<T extends Function>(fn: T): string;
+function nameof<T extends object>(obj: T): keyof T;
+function nameof<T extends object, K extends keyof T = keyof T>(key: K): K;
+function nameof<T extends object, K extends keyof T = keyof T>(
+  obj: T,
+  key: K
+): K;
 function nameof(arg1: any, arg2?: any): any {
   if (arguments.length === 2 && arg1 !== null && typeof arg2 === "string") {
     return arg2;
